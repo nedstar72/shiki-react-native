c19e99fc71c100d5bb2c9a0a9f37c39ba9ccb597
nedstar72 <torgashin99@gmail.com>
2025-10-25 22:13:41 +0500

    chore: Update deps

--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-native';
 
+// FIXME: Возникает следующий warning без причины:
+// https://github.com/storybookjs/storybook/blob/dbf7c18ce4a329127028784c61b6f0526f295458/code/core/src/test/testing-library.ts#L26
 const main: StorybookConfig = {
   stories: ['../src/**/*.stories.?(ts|tsx)'],
   addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
