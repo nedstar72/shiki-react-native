d57807581e54ecec7e099eb185fbf095615c2468
nedstar72 <torgashin99@gmail.com>
2025-11-03 00:27:15 +0500

    feat(storybook): Upgrade to v10

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ import { AppRegistry } from 'react-native';
 import 'reflect-metadata';
 import '@/shared/themes'; // Configure unistyles
 
+import Storybook from './.rnstorybook';
 import { name as appName } from './app.json';
 import { App } from './src/app';
 
-let AppEntry;
+const isStorybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true';
 
-if (process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true') {
-  AppEntry = require('./.rnstorybook').default;
-} else {
-  AppEntry = App;
-}
-
-AppRegistry.registerComponent(appName, () => AppEntry);
+AppRegistry.registerComponent(appName, () => (isStorybookEnabled ? Storybook : App));
