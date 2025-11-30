ec4800c7251dc0d7aa5492b20fd030886527ccab
nedstar72 <torgashin99@gmail.com>
2025-11-30 21:03:28 +0500

    feat(storybook): Add TestView stories

---
+++ src/shared/ui-kit/TestView/TestView.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TestView } from './index';
+
+const meta: Meta<typeof TestView> = {
+  title: 'TestView',
+  component: TestView,
+  parameters: {
+    noBackground: true,
+  },
+};
+
+type Story = StoryObj<typeof TestView>;
+
+export const Default: Story = {};
+
+export default meta;
